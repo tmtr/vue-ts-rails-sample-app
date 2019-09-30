62650ef234aa3d9c78269c5dfe732f5ec1ea7a9f
tmtr <tera@tmtr.jp>
2019-09-30 14:55:17 +0900

    `@`のパス解決をapp/javascript/に変換するエイリアスを設定

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,16 @@
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
 const { VueLoaderPlugin } = require('vue-loader')
+const { resolve } = require('path')
 const vue = require('./loaders/vue')
+
+environment.config.merge({
+  resolve: {
+    alias: {
+      '@': resolve('app/javascript')
+    }
+  }
+})
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
