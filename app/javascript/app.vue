de921a229b1c0e0d3d5c6ff980ebedc3e2e475ec
tmtr <tera@tmtr.jp>
2019-09-30 17:02:50 +0900

    動作確認のため '/' で vue を表示するように／js を ts 化

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -4,14 +4,23 @@
   </div>
 </template>
 
-<script>
-export default {
-  data: function () {
+<script lang="ts">
+import Vue from 'vue'
+
+export interface Todo {
+  id: number
+  task: string
+  done: boolean
+}
+
+export default Vue.extend({
+  data () {
     return {
-      message: "Hello Vue!"
+      message: "Hello Vue!",
+      todos: [] as Todo[]
     }
   }
-}
+})
 </script>
 
 <style scoped>
